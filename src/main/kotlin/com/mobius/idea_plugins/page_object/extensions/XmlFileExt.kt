7109fca997689583b665e1c1b95848e1965777d5
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-11-14 21:06:40 +0400

    Refactoring of XML tags info collecting

--- src/main/kotlin/com/mobius/idea_plugins/page_object/extensions/XmlFileExt.kt
+++ src/main/kotlin/com/mobius/idea_plugins/page_object/extensions/XmlFileExt.kt
@@ -3,48 +3,47 @@ package com.mobius.idea_plugins.page_object.extensions
 import android.databinding.tool.ext.toCamelCase
 import com.android.SdkConstants
 import com.android.tools.idea.psi.TagToClassMapper
+import com.intellij.psi.XmlRecursiveElementVisitor
 import com.intellij.psi.util.InheritanceUtil
-import com.intellij.psi.xml.XmlDocument
 import com.intellij.psi.xml.XmlFile
 import com.intellij.psi.xml.XmlTag
 import com.mobius.idea_plugins.page_object.model.AndroidTagDeclaration
 import com.mobius.idea_plugins.page_object.model.AndroidTagInfoModel
 import org.jetbrains.kotlin.idea.util.module
 
+private const val XML_FILE_EXTENSION = ".xml"
 
-val XmlFile.pageObjectClassName: String get() = name.removeSuffix(".xml").toCamelCase().capitalize()
+
+val XmlFile.pageObjectClassName: String get() = name.removeSuffix(XML_FILE_EXTENSION).toCamelCase().capitalize()
 
 fun XmlFile.collectAndroidTagsInfoList(): List<AndroidTagInfoModel> {
-    return document?.collectXmlTags()?.mapNotNull { it.toAndroidTagInfoModel() } ?: emptyList()
-}
+    val result = mutableListOf<AndroidTagInfoModel>()
 
+    this.accept(object : XmlRecursiveElementVisitor() {
+        override fun visitXmlTag(tag: XmlTag?) {
+            // DomManager doesn't work as expected, always returns null,
+            // because getDomElement returns LayoutViewElement, that cannot be converted into View ;(
+//            val tagPsiClass = (DomManager.getDomManager(project).getDomElement(tag) as? View)?.viewClass?.value
+            val tagPsiClass = tag?.module?.let {
+                TagToClassMapper.getInstance(it).getClassMap(SdkConstants.CLASS_VIEW)[tag.localName]
+            }
 
-private fun XmlDocument.collectXmlTags(): List<XmlTag> {
-    val result = mutableListOf<XmlTag>()
+            val idAttrValue = tag
+                ?.getAttributeValue("${SdkConstants.ANDROID_NS_NAME_PREFIX}${SdkConstants.ATTR_ID}")
+                ?.removePrefix(SdkConstants.NEW_ID_PREFIX)
 
-    rootTag?.collectTagsRecursively(result)
+            if (idAttrValue != null && tagPsiClass != null) {
+                val declaration = AndroidTagDeclaration.values().first { declaration ->
+                    declaration.widgetFQCNs.any { widgetFQCN -> InheritanceUtil.isInheritor(tagPsiClass, widgetFQCN) }
+                }
+                result += AndroidTagInfoModel(
+                    id = idAttrValue,
+                    androidTagDeclaration = declaration
+                )
+            }
+            super.visitXmlTag(tag)
+        }
+    })
 
     return result
-}
-
-private fun XmlTag.collectTagsRecursively(result: MutableList<XmlTag>) {
-    result += this
-    this.children.filterIsInstance<XmlTag>().forEach { it.collectTagsRecursively(result) }
-}
-
-private fun XmlTag.toAndroidTagInfoModel(): AndroidTagInfoModel? {
-    val idAttr = this.attributes.firstOrNull { it.localName == "id" }
-    val idAttrValue = idAttr?.value?.removePrefix(SdkConstants.NEW_ID_PREFIX) ?: return null
-
-    val tagPsiClass = TagToClassMapper.getInstance(this.containingFile?.module!!)
-        .getClassMap(SdkConstants.CLASS_VIEW)[localName] ?: return null
-
-    val declaration = AndroidTagDeclaration.values().first {
-        it.widgets.any { widget -> InheritanceUtil.isInheritor(tagPsiClass, widget) }
-    }
-
-    return AndroidTagInfoModel(
-        id = idAttrValue,
-        androidTagDeclaration = declaration
-    )
 }
